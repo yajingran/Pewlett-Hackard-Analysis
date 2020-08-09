e0a9172c45ec78393371c3be140fa5f14047cdb8
Yajing Ran <66695466+yajingran@users.noreply.github.com>
2020-08-09 10:49:50 -0400

    Add files via upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -34,5 +34,21 @@ GROUP BY ut.title
 ORDER BY count(ut.title) DESC;
 
 
-
+-- Delieverable 2
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+tt.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as tt
+ON (e.emp_no = tt.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY emp_no ASC;
 
